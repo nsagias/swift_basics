df54f8a265d934ceb320c825c54d171977e24aaf
Nick <nsagias@gmail.com>
2023-05-02 19:45:12 -0400

    add union example

--- sets.playground/Contents.swift
+++ sets.playground/Contents.swift
@@ -13,3 +13,10 @@ var numArrayExample = [1,2,3,4,4,5,5,5, 9, 10]
 
 var numArrayCovertedToSet: Set = Set(numArrayExample)
 print("Num Array Converted to Set: ", numArrayCovertedToSet)
+
+
+// Union Example
+var mySetExample1: Set = [1,2,3]
+var mySetExample2: Set = [3,4,5]
+var mySetUnionResult = mySetExample1.union(mySetExample2)
+print("MY SET UNION RESULT: ", mySetUnionResult)
