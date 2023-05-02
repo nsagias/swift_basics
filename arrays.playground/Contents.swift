2fb24f03f2dff437f9967e838d5d5d1c4991746a
Nick <nsagias@gmail.com>
2023-05-02 00:50:58 -0400

    add array functions and add comments

--- arrays.playground/Contents.swift
+++ arrays.playground/Contents.swift
@@ -2,3 +2,29 @@ import UIKit
 
 var myArrayExample = ["Hello", "playground", "array example" ,"example"]
 
+// return total elements in array
+myArrayExample.count
+
+// return first item in array
+myArrayExample.first
+
+// return boolean if there are values
+myArrayExample.isEmpty
+
+// return last item in array
+myArrayExample.last
+
+// add to end of array and return complete array
+myArrayExample.append("append example")
+
+// return last index postion, zero based array
+myArrayExample.endIndex
+
+// return first index position, zero based array
+myArrayExample.startIndex
+
+// join all the elments in the array and return
+myArrayExample.joined()
+
+// print entire array to command line
+print(myArrayExample)
