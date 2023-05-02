41404f33866d0a463ab0062dc8db16331acece25
Nick <nsagias@gmail.com>
2023-05-02 19:40:20 -0400

    add array converted to set example

--- sets.playground/Contents.swift
+++ sets.playground/Contents.swift
@@ -1,4 +1,15 @@
 import UIKit
 
 // sets
-var setExample : Set =  [1,2,3,4,5]
+var setExample: Set =  [1,2,3,4,5]
+
+setExample.insert(6)
+setExample.insert(1)
+print(setExample)
+
+var setExampleStrings: Set = ["a", "b", "c"]
+
+var numArrayExample = [1,2,3,4,4,5,5,5, 9, 10]
+
+var numArrayCovertedToSet: Set = Set(numArrayExample)
+print("Num Array Converted to Set: ", numArrayCovertedToSet)
