d086bdaa254f6805a4cc3f8c5a2fca521f1d2ce3
Nick <nsagias@gmail.com>
2023-05-02 00:28:04 -0400

    add built in string function examples

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -11,12 +11,45 @@ print(myNumber)
 
 
 var userName = "Nick"
+// add to end of string
+userName.append("a")
+
+// to lowercase
+userName.lowercased()
+
+// to uppercase
+userName.uppercased()
+
+// check if variable is empty
+userName.isEmpty
+
+// get number of chars in variable
+userName.count
+
+// loop through using built-in foreach
+userName.forEach {
+    char in print(char)
+    
+}
+// String.index no value
+userName.endIndex
+
+// redmove at index, requires String.Index
+userName.remove(at: userName.startIndex)
+
+// capitalize and return first letter
+userName.first?.uppercased()
+
+// capitalize and return string
+userName.capitalized
+
 
 // constant cannot be re-assigned
-
 // integer type
 let userNumber = 10
 
-// double double type
+// double type
 let pi = 3.14
+
+
 
