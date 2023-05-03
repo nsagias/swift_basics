9d2a97967ff8df256e3357cd1b5092a3d6fe15a6
Nick <nsagias@gmail.com>
2023-05-02 23:27:23 -0400

    add if and else if example

--- flowControl.playground/Contents.swift
+++ flowControl.playground/Contents.swift
@@ -2,10 +2,26 @@ import UIKit
 
 // Flow Control
 
-// If
+// if example
 
 let numValue = 5
 
 if numValue < 10 {
     print("Less than 10")
+}
+
+// if else example
+if numValue > 10 {
+    print("Greater than 10")
+} else  {
+    print("Less than 10")
+}
+
+// if else if example
+if numValue > 10 {
+    print("Greater than 10")
+} else if numValue < 10 && numValue == 5 {
+    print("Value is 5")
+} else {
+    print("Default message")
 }
