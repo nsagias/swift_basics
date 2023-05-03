43c4906cbe27adb33c7e2e7ec09b3a3b9785936c
Nick <nsagias@gmail.com>
2023-05-02 23:35:09 -0400

    create functions, print hellow world, and second example to pass params

--- functions.playground/Contents.swift
+++ functions.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 // Functions
+func printHelloWorld() {
+    print("Hello World")
+}
+
+printHelloWorld()
+
+func printStringPassed(word: String, num: Int) {
+    print("\(word) \(num)")
+}
+printStringPassed(word: "Hello World", num: 5)
