be79c25f60dfffbb26fda845007176d9f700047d
Nick <nsagias@gmail.com>
2023-05-02 23:41:42 -0400

    create square area function

--- functions.playground/Contents.swift
+++ functions.playground/Contents.swift
@@ -7,7 +7,17 @@ func printHelloWorld() -> Void {
 
 printHelloWorld()
 
+// Print params passed
 func printStringPassed(word: String, num: Int) -> Void {
     print("\(word) \(num)")
 }
 printStringPassed(word: "Hello World", num: 5)
+
+// Area of a square/rectangle return Double
+func squareArea(a: Double, b: Double) -> Double {
+    return a * b
+}
+// declare variable
+let squareResult: Double = squareArea(a: 5, b: 10)
+print(squareResult)
+
