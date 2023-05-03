11008d962e7674d95a6cd3034208ab35abf54e3f
Nick <nsagias@gmail.com>
2023-05-03 00:03:14 -0400

    add nullCoalescingExample

--- optionals.playground/Contents.swift
+++ optionals.playground/Contents.swift
@@ -6,3 +6,7 @@ myOptionalExample?.uppercased()
 
 // using exclamation point will it to wrap
 //myOptionalExample!.count
+let defaulValue = "I am the default value if nil/null"
+
+var nullCoalescingExample = myOptionalExample ?? defaulValue
+print(nullCoalescingExample)
